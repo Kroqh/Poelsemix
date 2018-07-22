d2a2c529c70358186fafd54354d7175b971cb4c7
Krogh <marcus.k@live.dk>
2018-07-22 14:16:01 +0200

    swe_kal force staff
    forcestaff meta inb

--- game/dota_addons/poelsemix/scripts/vscripts/items/item_force_staff.lua
+++ game/dota_addons/poelsemix/scripts/vscripts/items/item_force_staff.lua
@@ -16,6 +16,21 @@ function ForceStaff (keys)
 
 end
 
+function ForceStaffB (keys)
+
+	local target = keys.target
+	local ability = keys.ability
+	local ability_level = ability:GetLevel() - 1	
+
+	target:Stop()
+
+	ability.forced_direction = -target:GetForwardVector()
+	ability.forced_distance = ability:GetLevelSpecialValueFor("push_length", ability_level)
+	ability.forced_speed = ability:GetLevelSpecialValueFor("push_speed", ability_level) * 1/30	-- * 1/30 gives a duration of ~0.4 second push time (which is what the gamepedia-site says it should be)
+	ability.forced_traveled = 0
+
+end
+
 function ForceHorizontal( keys )
 
 	local target = keys.target
