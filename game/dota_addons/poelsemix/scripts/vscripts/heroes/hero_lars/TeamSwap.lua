9037dc79422942f895a3dd31e25cd66bb984c489
DaaseAllan <laurits.mumberg@gmail.com>
2018-07-23 14:44:43 +0200

    Teamswap giver nu vision til ham der bliver swappet

--- game/dota_addons/poelsemix/scripts/vscripts/heroes/hero_lars/TeamSwap.lua
+++ game/dota_addons/poelsemix/scripts/vscripts/heroes/hero_lars/TeamSwap.lua
@@ -1,12 +1,14 @@
 function SwapTeam( keys )
 	local caster = keys.caster
-	local target = keys.target	
+	local target = keys.target
+
+	AddFOWViewer(target:GetTeam(), target:GetAbsOrigin(), 5000, 5, false)
+
 	LarsTeamNumb = caster:GetTeamNumber()
 	TargetTeamNumb = target:GetTeamNumber()
 
 	target:SetTeam(LarsTeamNumb)
 	target:SetFriction(0)
-
 end
 
 function SwapBack( keys )
