c43b06f9f71a29ecc64fb03cfa4ffc6725557b0b
DaaseAllan <laurits.mumberg@gmail.com>
2018-07-23 14:10:06 +0200

    LarsFadbamse aktiveres ikke længere på allies. Lidt et nerf, men gør at han kan spilles i gamemodes med mere end 1 på hvert hold

--- game/dota_addons/poelsemix/scripts/vscripts/heroes/hero_lars/LarsDrunk.lua
+++ game/dota_addons/poelsemix/scripts/vscripts/heroes/hero_lars/LarsDrunk.lua
@@ -1,23 +1,28 @@
 function LarsDrunkStart( keys )
+	print("loool")
 	local caster = keys.caster
 	local target = keys.target
 
-		if target:IsAlive() then
-			local order = 
-			{
-				UnitIndex = caster:entindex(),
-				OrderType = DOTA_UNIT_ORDER_ATTACK_TARGET,
-				TargetIndex = caster:entindex()
-			}
-	
-			ExecuteOrderFromTable(order)
-		else
-			caster:Stop()
-		end
-	
-		-- Set the force attack target to be the caster (unit loooool)
-		caster:SetForceAttackTargetAlly(target)
-	-------------------------------------------
+	-- Clear the force attack target
+	caster:SetForceAttackTarget(nil)
+
+	-- Give the attack order if the caster is alive
+	-- otherwise forces the target to sit and do nothing
+	if target:IsAlive() then
+		local order = 
+		{
+			UnitIndex = caster:entindex(),
+			OrderType = DOTA_UNIT_ORDER_ATTACK_TARGET,
+			TargetIndex = target:entindex()
+		}
+
+		ExecuteOrderFromTable(order)
+	else
+		caster:Stop()
+	end
+
+	-- Set the force attack target to be the caster
+	caster:SetForceAttackTarget(target)
 end
 
 
