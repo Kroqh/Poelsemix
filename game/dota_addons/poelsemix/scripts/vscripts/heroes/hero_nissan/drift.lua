baffc9333c4f8675936c79124c1511e44e8ad15d
Steven <thedankpure@gmail.com>
2020-02-14 23:13:28 +0100

    nissan drift + runover done. model added.

---
+++ game/dota_addons/poelsemix/scripts/vscripts/heroes/hero_nissan/drift.lua
@@ -0,0 +1,110 @@
+LinkLuaModifier("modifier_drift_dummy", "heroes/hero_nissan/drift", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_drift_burn", "heroes/hero_nissan/drift", LUA_MODIFIER_MOTION_NONE)
+drift = class({})
+
+function drift:GetCastPoint()
+	local caster = self:GetCaster()
+
+	return self:GetSpecialValueFor("cast_point")
+end
+
+modifier_drift_dummy = class({})
+
+modifier_drift_burn = class({})
+
+function modifier_drift_burn:OnCreated()
+	if not IsServer() then return end
+
+	local ability = self:GetAbility()
+	local caster = self:GetCaster()
+
+	self.damage = ability:GetSpecialValueFor("damage_per_second")
+	self.tick = ability:GetSpecialValueFor("burn_tick_interval")
+	self:StartIntervalThink(self.tick)
+end
+
+function modifier_drift_burn:OnIntervalThink()
+	if not IsServer() then return end
+
+	local target = self:GetParent()
+	local caster = self:GetCaster()
+
+	ApplyDamage({
+				victim = target,
+				attacker = caster,
+				damage_type = DAMAGE_TYPE_MAGICAL,
+				damage = self.damage,
+				ability = self:GetAbility()
+	})
+end
+
+function drift:OnSpellStart()
+	if IsServer() ~= true then return end
+
+	--local ability = self:GetAbility()
+	local radius = self:GetSpecialValueFor("hit_radius")
+	local burn_duration = self:GetSpecialValueFor("burn_duration")
+
+	local caster = self:GetCaster()
+	local target_pos = self:GetCursorPosition()
+	local caster_pos = caster:GetAbsOrigin()
+
+	local dash_length = self:GetSpecialValueFor("dash_length")
+	local dash_width = self:GetSpecialValueFor("dash_width")
+	local dash_duration = self:GetSpecialValueFor("dash_duration")
+
+	local direction = (target_pos - caster_pos):Normalized()
+
+	caster:SetForwardVector(direction)
+
+	local forward_direction = caster:GetForwardVector()
+  local right_direction = caster:GetRightVector()
+  local caster_angles = caster:GetAngles()
+
+	local start_time = GameRules:GetGameTime()
+	local ellipse_center = caster_pos + forward_direction * (dash_length / 2)
+
+	local pfx = ParticleManager:CreateParticle( "particles/units/heroes/hero_phoenix/phoenix_icarus_dive.vpcf", PATTACH_WORLDORIGIN, nil )
+
+  caster:AddNewModifier(caster, self, "modifier_drift_dummy", {duration = dash_duration})
+
+	caster:SetContextThink(DoUniqueString("drift_update"), function() 
+		ParticleManager:SetParticleControl(pfx, 0, caster:GetAbsOrigin() + caster:GetRightVector() * 32 )
+		if not caster:HasModifier("modifier_drift_dummy") then
+			ParticleManager:DestroyParticle(pfx, false)
+			ParticleManager:ReleaseParticleIndex(pfx)
+			caster:SetAbsOrigin(caster_pos)
+	    return nil 
+		end
+		-- ellipse calculations
+		local elapsed_time = GameRules:GetGameTime() - start_time
+		local progress = elapsed_time / dash_duration
+
+		self.progress = progress
+		-- thanks dota imba for fixing
+		local t = -dash_duration * math.pi * progress
+		local x = math.sin(t) * dash_width  * 0.5
+		local y = -math.cos(t) * dash_length * 0.5
+
+		local pos = ellipse_center + right_direction * x + forward_direction * y
+
+		-- drift effect
+		local yaw = caster_angles.y + progress * -360
+
+		pos = GetGroundPosition(pos, caster)
+		
+		caster:SetAbsOrigin(pos)
+		caster:SetAngles(caster_angles.x, yaw, caster_angles.z)
+
+		GridNav:DestroyTreesAroundPoint(pos, 80, false)
+
+		-- modifier things
+		local units = FindUnitsInRadius(caster:GetTeamNumber(), caster:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_ENEMY, 
+																		DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
+		for _, enemy in pairs(units) do 
+			enemy:AddNewModifier(self:GetCaster(), self, "modifier_drift_burn", {duration = burn_duration})
+		end
+
+		return 0.03
+	end, 0)
+end
---
+++ game/dota_addons/poelsemix/scripts/vscripts/heroes/hero_nissan/runover.lua
@@ -0,0 +1,132 @@
+LinkLuaModifier("modifier_runover", "heroes/hero_nissan/runover", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_runover_stun", "heroes/hero_nissan/runover", LUA_MODIFIER_MOTION_NONE)
+
+runover = class({})
+
+function runover:OnSpellStart()
+  if not IsServer() then return end
+  local caster = self:GetCaster()
+  local duration = self:GetSpecialValueFor("duration")
+
+  caster:AddNewModifier(caster, self, "modifier_runover", {duration = duration})
+end
+
+modifier_runover = class({})
+
+function modifier_runover:IsHidden() return false end
+function modifier_runover:IsDebuff() return false end
+
+function modifier_runover:OnCreated()
+  self.movespeed = self:GetAbility():GetSpecialValueFor("bonus_movespeed")
+  local caster = self:GetCaster()
+
+  if not IsServer() then return end
+  self.particle = "particles/units/heroes/hero_techies/techies_suicide_base.vpcf"
+  local pfx = ParticleManager:CreateParticle(self.particle, PATTACH_ABSORIGIN_FOLLOW, caster)
+  ParticleManager:SetParticleControl(pfx, 0, caster:GetAbsOrigin())
+  ParticleManager:SetParticleControl(pfx, 2, Vector(1.5,1.5,1.5))
+  
+  caster:EmitSound("Hero_Techies.Suicide")
+
+  self:StartIntervalThink(0.1)
+end
+
+function modifier_runover:DeclareFunctions()
+  return {MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE}
+end
+
+function modifier_runover:GetModifierMoveSpeedBonus_Percentage()
+  return self.movespeed
+end
+
+function modifier_runover:OnIntervalThink()
+  if not IsServer() then return end
+  
+  local ability = self:GetAbility()
+  local caster = ability:GetCaster()
+
+  local radius = ability:GetSpecialValueFor("radius")
+  local stun_duration = ability:GetSpecialValueFor("stun_duration")
+  local damage = ability:GetSpecialValueFor("damage")
+
+  local enemies = FindUnitsInRadius(caster:GetTeamNumber(), 
+                                    caster:GetAbsOrigin(), 
+                                    nil, 
+                                    radius, 
+                                    DOTA_UNIT_TARGET_TEAM_ENEMY, 
+                                    DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
+                                    DOTA_UNIT_TARGET_FLAG_NONE, 
+                                    FIND_ANY_ORDER, false)
+  
+  for _, enemy in pairs(enemies) do 
+    if not enemy:HasModifier("modifier_runover_stun") then
+      -- Particles
+      local pfx = ParticleManager:CreateParticle(self.particle, PATTACH_ABSORIGIN_FOLLOW, caster)
+      ParticleManager:SetParticleControl(pfx, 0, caster:GetAbsOrigin())
+      ParticleManager:SetParticleControl(pfx, 2, Vector(1.5,1.5,1.5))
+
+      ApplyDamage({
+				victim = enemy,
+				attacker = caster,
+				damage_type = DAMAGE_TYPE_MAGICAL,
+				damage = damage,
+				ability = ability
+	    })
+      enemy:AddNewModifier(caster, ability, "modifier_runover_stun", {duration = stun_duration})
+      
+      caster:EmitSound("Hero_Techies.Suicide")
+    end
+  end
+end
+
+modifier_runover_stun = class({})
+
+function modifier_runover_stun:IsPurgeable() return false end
+function modifier_runover_stun:IsHidden() return true end
+
+function modifier_runover_stun:GetEffectName()
+	return "particles/generic_gameplay/generic_stunned.vpcf"
+end
+
+function modifier_runover_stun:OnCreated()
+  if not IsServer() then return end
+  self.angles = self:GetParent():GetAngles()
+  self.ability = self:GetAbility()
+
+  self:GetParent():SetAngles(-90, self.angles.y, self.angles.z)
+end
+
+function modifier_runover_stun:GetEffectAttachType()
+	return PATTACH_OVERHEAD_FOLLOW
+end
+
+function modifier_runover_stun:CheckState()
+	local state = {[MODIFIER_STATE_STUNNED] = true}
+	return state
+end
+
+function modifier_runover_stun:OnDestroy()
+  if not IsServer() then return end
+  local parent = self:GetParent()
+  local angles = parent:GetAngles()
+  local radius = self.ability:GetSpecialValueFor("radius")
+  local found = false;
+  local units = FindUnitsInRadius(parent:GetTeamNumber(), 
+                                  parent:GetAbsOrigin(), 
+                                  nil, 
+                                  radius, 
+                                  DOTA_UNIT_TARGET_TEAM_ENEMY, 
+                                  DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
+                                  DOTA_UNIT_TARGET_FLAG_NONE, 
+                                  FIND_ANY_ORDER, false)
+  
+  for _, enemy in pairs(units) do
+    if enemy:GetUnitName() == "npc_dota_hero_ogre_magi" then found = true end;
+  end
+
+  if found then
+    parent:SetAngles(-90, angles.y, angles.z)
+  else
+    parent:SetAngles(0, angles.y, angles.z)
+  end
+end
