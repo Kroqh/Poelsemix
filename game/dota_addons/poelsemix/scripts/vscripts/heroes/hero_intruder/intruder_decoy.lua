800b295703b47df45ab7dc524341c75464730249
Marcus Krogh <37517255+Kroqh@users.noreply.github.com>
2023-02-22 17:26:44 +0100

    kaz model and first illusions fix

--- game/dota_addons/poelsemix/scripts/vscripts/heroes/hero_intruder/intruder_decoy.lua
+++ game/dota_addons/poelsemix/scripts/vscripts/heroes/hero_intruder/intruder_decoy.lua
@@ -4,42 +4,20 @@ LinkLuaModifier("modifier_intruder_stealth", "heroes/hero_intruder/modifier_intr
 function intruder_decoy:OnSpellStart()
     if not IsServer() then return end
     local caster = self:GetCaster()
-    local unit_name = caster:GetUnitName()
-    local origin = caster:GetAbsOrigin() + RandomVector(50)
     caster:AddNewModifier(caster, ability, "modifier_intruder_stealth", {duration = self:GetSpecialValueFor("invis_dur")})
 
-    local illusion = CreateUnitByName(unit_name, origin, true, caster, nil, caster:GetTeamNumber())
-	illusion:SetPlayerID(caster:GetPlayerID())
+	local illusions = CreateIllusions(caster, caster, {
+		outgoing_damage = self:GetSpecialValueFor("clone_outgoing"),
+		incoming_damage	= 0,
+		bounty_base		= 0,
+		bounty_growth	= nil,
+		outgoing_damage_structure	= nil,
+		outgoing_damage_roshan		= nil,
+		duration		= self:GetSpecialValueFor("clone_dur")
 
-    local targetLevel = caster:GetLevel()
-	for i=1,targetLevel-1 do
-		illusion:HeroLevelUp(false)
+	}, 1, 0, false, true)
+
+	for _, illusion in pairs(illusions) do
+		illusion:SetControllableByPlayer(-1, true) -- uncontrollable
 	end
-
-    illusion:SetAbilityPoints(0)
-	for abilitySlot=0,15 do
-		local ability = caster:GetAbilityByIndex(abilitySlot)
-		if ability ~= nil then 
-			local abilityLevel = ability:GetLevel()
-			local abilityName = ability:GetAbilityName()
-			local illusionAbility = illusion:FindAbilityByName(abilityName)
-			illusionAbility:SetLevel(abilityLevel)
-		end
-	end
-
-	-- Recreate the items of the target
-	for itemSlot=0,5 do
-		local item = caster:GetItemInSlot(itemSlot)
-		if item ~= nil then
-			local itemName = item:GetName()
-			local newItem = CreateItem(itemName, illusion, illusion)
-			illusion:AddItem(newItem)
-		end
-	end
-
-
-    illusion:AddNewModifier(caster, ability, "modifier_illusion", { duration = self:GetSpecialValueFor("clone_dur"), outgoing_damage = self:GetSpecialValueFor("clone_outgoing"), incoming_damage = 0 })
-	
-	-- Without MakeIllusion the unit counts as a hero, e.g. if it dies to neutrals it says killed by neutrals, it respawns, etc.
-	illusion:MakeIllusion()
 end
