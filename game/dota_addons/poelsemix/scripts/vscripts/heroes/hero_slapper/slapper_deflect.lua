522c60db6584bd4a202c50a08803f9adcb7558e2
Marcus Krogh <37517255+Kroqh@users.noreply.github.com>
2024-05-20 15:00:05 +0200

    Deflect

---
+++ game/dota_addons/poelsemix/scripts/vscripts/heroes/hero_slapper/slapper_deflect.lua
@@ -0,0 +1,90 @@
+LinkLuaModifier("modifier_slapper_deflect_passive", "heroes/hero_slapper/slapper_deflect", LUA_MODIFIER_MOTION_NONE)
+slapper_deflect = slapper_deflect or class({})
+
+
+function slapper_deflect:GetIntrinsicModifierName()
+	return "modifier_slapper_deflect_passive"
+end
+
+
+function slapper_deflect:OnProjectileHit(target)
+	if not target then
+		return nil 
+	end
+	local caster = self:GetCaster()
+	local int_scaling = self:GetSpecialValueFor("int_damage")
+	local base_damage = self:GetSpecialValueFor("damage")
+	local damage = base_damage + (int_scaling * caster:GetIntellect())
+	ApplyDamage({victim = target,
+	attacker = caster,
+	damage_type = self:GetAbilityDamageType(),
+	damage = damage,
+	ability = self})
+end
+
+modifier_slapper_deflect_passive = modifier_slapper_deflect_passive or class({})
+
+function modifier_slapper_deflect_passive:IsPurgeable() return false end
+function modifier_slapper_deflect_passive:IsHidden() return false end
+function modifier_slapper_deflect_passive:IsPassive() return true end
+
+function modifier_slapper_deflect_passive:OnCreated()
+	if IsServer() then
+		local ability = self:GetAbility()
+		local lol = 0
+
+		if lol == 0 then
+			local caster = self:GetCaster()
+			self:SetStackCount(1)
+			lol = 1
+		end
+
+		self:StartIntervalThink(ability:GetSpecialValueFor("interval"))
+	end
+end
+
+function modifier_slapper_deflect_passive:OnIntervalThink()
+	if IsServer() then
+		local ability = self:GetAbility()
+
+		local max_stacks = ability:GetSpecialValueFor("max_charges")
+
+		if self:GetStackCount() < max_stacks then
+			self:SetStackCount(self:GetStackCount() + 1)
+		end
+
+		self:StartIntervalThink(ability:GetSpecialValueFor("interval"))
+	end
+end
+
+
+function modifier_slapper_deflect_passive:DeclareFunctions()
+	local decFuncs = {MODIFIER_EVENT_ON_ATTACK_LANDED}
+	return decFuncs
+end
+
+
+function modifier_slapper_deflect_passive:OnAttackLanded( keys )
+	local parent = self:GetParent()
+	if keys.target == parent and IsServer() then
+		local stacks =self:GetStackCount()
+		if stacks == 0 then return end
+		self:SetStackCount(stacks-1)
+		parent:StartGesture(ACT_DOTA_CAST_ABILITY_2)
+		local shot = 
+			{
+				Target = keys.attacker,
+				Source = parent,
+				Ability = self:GetAbility(),
+				EffectName = "particles/units/heroes/hero_slapperl/slapper_deflect.vpcf",
+				iMoveSpeed = self:GetAbility():GetSpecialValueFor("proj_speed"),
+				bDodgeable = true,
+				bVisibleToEnemies = true,
+				bReplaceExisting = false,
+				bProvidesVision = false,
+				ExtraData = {}
+			}
+		ProjectileManager:CreateTrackingProjectile(shot)
+	end
+end
+
